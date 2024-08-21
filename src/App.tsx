cb6926c88a421219371a1ff056129377e9635e5a
Od4ir <od4ir@usp.br>
2024-08-20 22:26:57 -0300

    style: Animação das logos na página de standby

--- src/App.tsx
+++ src/App.tsx
@@ -2,31 +2,31 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import od4irLogo from './assets/logo_od4ir_principal.svg'
+import od4irLogoRascunho from './assets/logo_od4ir_rascunho.svg'
+import od4irLogoIcone from './assets/logo_od4ir_icone.svg'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='logo-home-container'>
+        <img className='logo-home-icone' src={od4irLogoIcone} alt="Ícone logo do Odair" />
+        <img className='logo-home' src={od4irLogo} alt="Logo do Site do Odair" />
+        <img className='logo-home-rascunho' src={od4irLogoRascunho} alt="Rascunho da logo do Site do Odair" />
       </div>
-      <h1>Vite + React</h1>
+      <h1>OD41R'S SITE</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+          Cliques: {count}
         </button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        Em breve, meu site :)
       </p>
     </>
   )
