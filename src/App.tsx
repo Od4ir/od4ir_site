b0712a2a9ac27c3781e12fb6a95960f16aa09ba0
Od4ir <od4ir@usp.br>
2024-08-20 22:33:27 -0300

    fix: Remoção de imports desnecessários na App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import od4irLogo from './assets/logo_od4ir_principal.svg'
 import od4irLogoRascunho from './assets/logo_od4ir_rascunho.svg'
